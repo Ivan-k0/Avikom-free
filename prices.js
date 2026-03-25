b0c5a22a546aa268c0604a458ec96fa8d8864734
Ivan-k0 <ivk1989@gmail.com>
2026-03-25 21:31:22 +0200

    Add files via upload

--- prices.js
+++ prices.js
@@ -5,7 +5,7 @@
 var PRICES = {
   aly_perv: 123, aly_prof: 106, aly_mix: 86, aly_mot: 86,
   aly_rad: 55, aly_bank: 72, zam: 56, mag: 25,
-  med: 450, med_blesk: 470,
+  med: 460, med_blesk: 470,
   latun: 255,
   nerj_10: 26, nerj_8: 22,
   akb_w: 24, akb_wz: 20, akb_gel: 17, akb_eb: 14,
