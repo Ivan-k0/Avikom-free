27929b51fb49c9353c817dd48231f724ac6afe68
Ivan-k0 <ivk1989@gmail.com>
2026-03-26 23:35:26 +0200

    Update prices.js

--- prices.js
+++ prices.js
@@ -6,7 +6,7 @@ var PRICES = {
   aly_perv: 123, aly_prof: 106, aly_mix: 86, aly_mot: 86,
   aly_rad: 55, aly_bank: 72, zam: 56, mag: 25,
   med: 460, med_blesk: 470,
-  latun: 255,
+  latun: 260,
   nerj_10: 26, nerj_8: 22,
   akb_w: 24, akb_wz: 20, akb_gel: 17, akb_eb: 14,
   akb_tnz_b: 15, akb_tnz_m: 13, akb_tnz_k: 7,
